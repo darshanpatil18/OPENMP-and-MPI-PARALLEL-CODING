7e4087fcb3da0384cfffb1131f7276f38261c841
Patil Darshan Rajkumar <61110106+darshanpatil18@users.noreply.github.com>
2022-01-20 15:02:38 +0530

    Add files via upload

---
+++ mat_vec_mult.c
@@ -0,0 +1,44 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <sys/time.h>
+
+
+#define row 4
+
+int A[row][row];
+int B[row][row];
+int C[row][row];
+
+int main() 
+{
+    int i,j,k;
+    struct timeval tv1, tv2;
+    struct timezone tz;
+	double elapsed; 
+    omp_set_num_threads(omp_get_num_procs());
+    for (i= 0; i< row; i++)
+        for (j= 0; j< row; j++)
+	{
+		printf("Enter a Number for 'A' Matrix for the Row and Column : ");
+		scanf("%d", &A[i][j]);
+    	printf("Enter a Number for 'B' Matrix for the Row and Column : ");
+		scanf("%d", &B[i][j]);
+       	}
+    gettimeofday(&tv1, &tz);
+    #pragma omp parallel for private(i,j,k) shared(A,B,C)
+    for (i = 0; i < row; ++i) {
+        for (j = 0; j < row; ++j) {
+            for (k = 0; k < row; ++k) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+
+    gettimeofday(&tv2, &tz);
+    elapsed = (double) (tv2.tv_sec-tv1.tv_sec) + (double) (tv2.tv_usec-tv1.tv_usec) * 1.e-6;
+    printf("Elapsed time = %f Seconds.\n", elapsed);
+
+}
